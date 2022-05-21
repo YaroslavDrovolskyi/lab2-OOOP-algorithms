3f670696751974ff7fca7626303f35ba3595df5f
YarikDrovolsky <yar.drovolsky@gmail.com>
2022-05-21 18:05:48 +0300

    Edited my sorting algorithms classes - added overriding of some baseclass methods (getname(), getmemory()).
    Also added implementation of Singleton for them

--- lib/algoslib.h
+++ lib/algoslib.h
@@ -3,6 +3,7 @@
 #define ALGOSLIB_H
 
 #include "algoslib_global.h"
+#include <iostream>
 #include <string>
 #include <vector>
 #include <memory>
@@ -373,6 +374,192 @@ private:
 };
 
 
+
+
+
+
+
+
+template <typename T>
+void print(T* arr, std::size_t size) {
+    for (std::size_t i = 0; i < size; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+
+template <typename T, typename Comparator>
+class ALGOSLIB_EXPORT CombSort : public sortingalgo<Comparator> {
+private:
+    std::string name;
+    uint32_t memory;
+
+    T* arr;
+    std::size_t size;
+
+public:
+    virtual void run() override;
+    virtual std::string getname() override;
+    uint32_t getmemory() override;
+    std::vector<T> getvalues();
+    virtual ~CombSort();
+
+    static  CombSort* GetInstance(std::vector<T>& v, Comparator c);
+    static  CombSort* GetInstance(Comparator c);
+
+    CombSort() = delete;
+    CombSort(CombSort<T,Comparator>& other) = delete;
+    void operator=(const CombSort<T,Comparator> &) = delete;
+protected:
+    static inline CombSort<T,Comparator>* combsort_;
+
+private:
+    CombSort(std::vector<T>& arr, Comparator comparator);
+    CombSort(Comparator comparator);
+    void sort(T* arr, std::size_t size);
+};
+
+
+template <typename T, typename Comparator>
+class ALGOSLIB_EXPORT ShellSort : public sortingalgo<Comparator> {
+private:
+    std::string name;
+    uint32_t memory;
+
+    T* arr;
+    std::size_t size;
+
+public:
+    virtual void run() override;
+    virtual std::string getname() override;
+    uint32_t getmemory() override;
+    std::vector<T> getvalues();
+    virtual ~ShellSort() override;
+
+    static  ShellSort* GetInstance(std::vector<T>& v, Comparator c);
+    static  ShellSort* GetInstance(Comparator c);
+
+    ShellSort() = delete;
+    ShellSort(ShellSort<T,Comparator>& other) = delete;
+    void operator=(const ShellSort<T,Comparator> &) = delete;
+protected:
+    static inline ShellSort<T,Comparator>* shellsort_;
+
+private:
+    ShellSort(std::vector<T>& arr, Comparator comparator);
+    ShellSort(Comparator comparator);
+    void sort(T* arr, std::size_t size);
+    void sortInsertion(T* arr, std::size_t size);
+};
+
+template <typename T, typename Comparator>
+class ALGOSLIB_EXPORT OddEvenSort : public sortingalgo<Comparator> {
+private:
+    std::string name;
+    uint32_t memory;
+
+    T* arr;
+    std::size_t size;
+
+public:
+    virtual void run() override;
+    virtual std::string getname() override;
+    uint32_t getmemory() override;
+    std::vector<T> getvalues();
+    virtual ~OddEvenSort() override;
+
+    static  OddEvenSort* GetInstance(std::vector<T>& v, Comparator c);
+    static  OddEvenSort* GetInstance(Comparator c);
+
+    OddEvenSort() = delete;
+    OddEvenSort(OddEvenSort<T,Comparator>& other) = delete;
+    void operator=(const OddEvenSort<T,Comparator> &) = delete;
+protected:
+    static inline OddEvenSort<T,Comparator>* oddevensort_;
+
+private:
+    OddEvenSort(std::vector<T>& arr, Comparator comparator);
+    OddEvenSort(Comparator comparator);
+    void sort(T* arr, std::size_t size);
+};
+
+template <typename T, typename Comparator>
+class ALGOSLIB_EXPORT CocktailShakerSort : public sortingalgo<Comparator> {
+private:
+    std::string name;
+    uint32_t memory;
+
+    T* arr;
+    std::size_t size;
+
+public:
+    virtual void run() override;
+    virtual std::string getname() override;
+    uint32_t getmemory() override;
+    std::vector<T> getvalues();
+    virtual ~CocktailShakerSort() override;
+
+    static  CocktailShakerSort* GetInstance(std::vector<T>& v, Comparator c);
+    static  CocktailShakerSort* GetInstance(Comparator c);
+
+    CocktailShakerSort() = delete;
+    CocktailShakerSort(CocktailShakerSort<T,Comparator> &other) = delete;
+    void operator=(const CocktailShakerSort<T,Comparator> &) = delete;
+protected:
+    static inline CocktailShakerSort<T,Comparator>* cocktailshackersort_;
+
+private:
+    CocktailShakerSort(std::vector<T>& arr, Comparator comparator);
+    CocktailShakerSort(Comparator comparator);
+    void sort(T* arr, std::size_t size);
+};
+
+template <typename T, typename Comparator>
+class ALGOSLIB_EXPORT TimSort : public sortingalgo<Comparator> {
+private:
+    std::string name;
+    uint32_t memory;
+
+    T* arr;
+    std::size_t size;
+
+public:
+    virtual void run() override;
+    virtual std::string getname() override;
+    uint32_t getmemory() override;
+    std::vector<T> getvalues();
+    virtual ~TimSort() override;
+
+    static  TimSort* GetInstance(std::vector<T>& v, Comparator c);
+    static  TimSort* GetInstance(Comparator c);
+
+    TimSort() = delete;
+    TimSort(TimSort<T,Comparator> &other) = delete;
+    void operator=(const TimSort<T,Comparator> &) = delete;
+protected:
+    static inline TimSort<T,Comparator>* timsort_;
+
+private:
+    TimSort(std::vector<T>& arr, Comparator comparator);
+    TimSort(Comparator comparator);
+    void sort(T* arr, std::size_t size);
+    void insertionSort(T* arr, std::size_t start, std::size_t end);
+
+    struct Run {
+        std::size_t begin;
+        std::size_t size;
+
+        Run() : begin(0), size(0) {}
+        Run(std::size_t begin, std::size_t size) : begin(begin), size(size) {}
+    };
+
+    std::size_t getMinRunSize(std::size_t array_size);
+
+    void tryMerge(T* arr, std::vector<Run>& runs);
+    void fullMerge(T* arr, std::vector<Run>& runs);
+    void merge(T* arr, std::size_t left_begin, std::size_t left_end, std::size_t right_begin, std::size_t right_end);
+};
 
 
 
